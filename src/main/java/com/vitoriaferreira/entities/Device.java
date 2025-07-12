fb24118e384f21baefd9b038a5f599d4f661592d
Vitoria Ferreira <vitoriaferreira@alunos.utfpr.edu.br>
2025-07-12 15:11:29 -0300

    feat:criação de entidade Device e User

--- src/main/java/com/vitoriaferreira/entities/Device.java
+++ src/main/java/com/vitoriaferreira/entities/Device.java
@@ -1,5 +1,99 @@
 package com.vitoriaferreira.entities;
 
-public class Device {
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Device implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nameService;
+    private LocalDate dateService;
+    private String status;
+
+    private User user;
+
+    // Default constructor
+    public Device() {
+    }
+
+    // Constructor
+    public Device(Integer id, String nameService, LocalDate dateService, String status, User user) {
+        this.id = id;
+        this.nameService = nameService;
+        this.dateService = dateService;
+        this.status = status;
+        this.user = user;
+    }
+
+    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNameService() {
+        return nameService;
+    }
+
+    public void setNameService(String nameService) {
+        this.nameService = nameService;
+    }
+
+    public LocalDate getDateService() {
+        return dateService;
+    }
+
+    public void setDateService(LocalDate dateService) {
+        this.dateService = dateService;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Device other = (Device) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 
 }
